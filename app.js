e24c7325d4f28edeb9ebba818adc8a384702143c
Huseyn <abbasovhuseyn670@gmail.com>
2024-03-27 08:21:17 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+let num = +prompt("Number");
+
+for (let i = 2; i <= Math.floor(num / 2); i++) {
+    if (num % i !== 0) {
+        console.log(`${num} murekkeb  ededdir`);
+        break;
+    } else {
+        console.log(`${num} sade ededdir`);
+    }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+// let cem = 0;
+
+// for (let i = 1; i <= num / 2; i++) {
+//     if (num % i == 0) {
+//         cem += i;
+//     }
+// }
+// cem += num;
+// if (cem === num * 2) {
+//     console.log(`${num} mukemmel ededdir`);
+// } else {
+//     console.log(`${num} mukemmel eded deyil...`);
+// }
+
+
+// ---------------------------------------------------------------------------------------------------
+
+// let num1 = +prompt("Number1:");
+// let num2 = +prompt("Number2:");
+
+// let ferq = 0;
+// for ( ferq = num1; ferq <= num2; ferq++) { 
+// }  
+
+// let cem = 0;
+
+// for (let i = 1; i <= ferq / 2; i++) {
+//     if (ferq % i == 0) {
+//         cem += i;
+//     }
+// }
+// cem += ferq;
+// if (cem === ferq * 2) {
+//     console.log(`${ferq} mukemmel ededdir`);
+// } else {
+//     console.log(`${ferq} mukemmel eded deyil...`);
+// }
+
+
+
+
+
+// let num = +prompt("Number");
+
+// for (let i = 2; i <= num / 2; i++) {
+//     if (num % i == 0) {
+//         console.log(`${num} murekkeb  ededdir`);
+//         break;
+//     } else {
+//         console.log(`${num} sade ededdir`);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
